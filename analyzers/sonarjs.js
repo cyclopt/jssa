d6bdc74549b5e823f3bced91dda2b2426c663a54
Mixalis Papamixail <papa.mich@hotmail.com>
2018-01-09 15:55:06 +0200

    Added sonarjs analyzer

---
+++ analyzers/sonarjs.js
@@ -0,0 +1,36 @@
+/**
+ * This file contains code that runs sonarjs static analyzer
+ */
+
+// Load libraries 
+const { analyze } = require("sonarjs");
+
+// Function for logging purposes
+function log(message) {
+  console.log(message);
+}
+
+function onStart() {
+  console.log("Sonarjs analysis started");
+}
+ 
+function onEnd() {
+  console.log("Sonarjs analysis finished");
+}
+
+// Run analyzer
+async function runSonarJS(project_path, exclusions) {
+  const issues = await analyze(project_path, { log, onStart, onEnd, exclusions: exclusions });  
+  return issues;
+}
+
+module.exports = {
+  // Perfrorm analysis
+  analysis: function (project_path) {
+    return new Promise((resolve, reject) => {
+      // Run sonarjs
+      issues = runSonarJS(project_path, "");
+      resolve(issues);
+    });
+  }
+};
