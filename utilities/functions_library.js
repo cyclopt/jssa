a6447a8c002ec4f3f612f9919d8fc6062005bd6d
Mixalis Papamixail <papa.mich@hotmail.com>
2018-06-01 18:38:20 +0300

    Added get list of js files functionality

--- utilities/functions_library.js
+++ utilities/functions_library.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const walk = require('walk');
+const path = require('path');
 
 module.exports = {
   readCode: function (pathToCode) {
@@ -12,5 +14,21 @@ module.exports = {
       path: pathToCode,
       code: fs.readFileSync(pathToCode).toString().trim(),
     };
+  },
+  get_list_of_js_files: function (root_dir){
+
+    return new Promise((resolve, reject) => {
+      walker = walk.walk(root_dir, { filters: ["node_modules"] });
+      var list_of_files = [];
+      walker.on("file", function (root, fileStats, next) {
+        if(fileStats.name.endsWith('.js')){
+          list_of_files.push(root + path.sep + fileStats.name);
+        }
+        next();
+      });
+      walker.on("end", function () {
+        resolve(list_of_files);
+      });
+    });
   }
 };
