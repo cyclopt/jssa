45b49fa264f5b70e2f16ccd85b68731c3cf91207
MichaelPap <papa.mich@hotmail.com>
2018-08-24 17:37:45 +0300

    Added npm audit analysis #2

--- analysis.js
+++ analysis.js
@@ -4,11 +4,14 @@
 
 // Load libraries
 const _ = require('lodash');
+const fs = require('fs');
+
 
 // Load analyzers
 const escomplex_analysis = require('./analyzers/escomplex');
 const eslint_analysis = require('./analyzers/eslint');
 const nsp_analysis = require('./analyzers/nsp');
+const npmaudit_analysis = require('./analyzers/npmaudit');
 const jsinspect_analysis = require('./analyzers/jsinspect');
 const sonarjs_analysis = require('./analyzers/sonarjs');
 
@@ -32,6 +35,26 @@ function nsp(project){
   // If spaces are contained in the path, then they need to be escaped.
   var projectAbsPath = project.replace(' ', '\" \"');
   return nsp_analysis.analysis(projectAbsPath);
+}
+
+function npmaudit(project){
+  
+  if(fs.existsSync(project + '/package.json') && fs.existsSync(project + '/package-lock.json')){
+    const package = JSON.stringify(require(project + '/package.json'))
+    const packageLock = JSON.stringify(require(project + '/package-lock.json'))
+
+    return npmaudit_analysis.analysis(package, packageLock);
+  }
+  else{
+    if(fs.existsSync(project + '/package.json')){
+      return {"npmaudit": {"error": "package.json not found"}};
+    }
+    else{
+      return {"npmaudit": {"error": "package-lock.json not found"}};
+    }
+    
+  }
+  
 }
 
 function jsinspect(list_of_files){
@@ -84,6 +107,11 @@ module.exports = {
       resolve(nsp(project_root));
     });
   },
+  analyze_npmaudit: function(project_root){
+    return new Promise((resolve, reject) => {
+      resolve(npmaudit(project_root));
+    });
+  },
   analyze_jsinspect: function(list_of_files){
     return new Promise((resolve, reject) => {
       resolve(jsinspect(list_of_files));
@@ -95,3 +123,51 @@ module.exports = {
     });
   }
 };
+
+
+// module.exports.analyze_all('example_project' ,['example_project/routes/index.js']).then(res =>{
+//   console.log(JSON.stringify(res))
+// })
+
+module.exports.analyze_npmaudit('./').then(res =>{
+  console.log('npmaudit')
+  console.log(JSON.stringify(res))
+})
+
+// module.exports.analyze_sonarjs('example_project').then(res =>{
+//   console.log('sonar')
+//   console.log(JSON.stringify(res))
+// })
+
+// module.exports.analyze_escomplex(['example_project/routes/index.js']).then(res =>{
+//   console.log('escomplex')
+//   console.log(JSON.stringify(res))
+// })
+
+// module.exports.analyze_jsinspect(['example_project/routes/index.js']).then(res =>{
+//   console.log('jsinspect')
+//   console.log(JSON.stringify(res))
+// })
+
+// module.exports.analyze_nsp('example_project').then(res =>{
+//   console.log('nsp')
+//   console.log(JSON.stringify(res))
+// })
+
+// module.exports.analyze_eslint(['example_project/routes/index.js']).then(res =>{
+//   console.log('eslint')
+//   console.log(JSON.stringify(res))
+// })
+
+
+// const package = JSON.stringify(require('./pacage.json'));
+// const packageLock = JSON.stringify(require('./package-lock.json'));
+ 
+
+// const runNpmAudit = require('run-npm-audit');
+ 
+// const package = JSON.stringify(require('./package.json'));
+// const packageLock = JSON.stringify(require('./package-lock.json'));
+ 
+// const report = runNpmAudit({ package, packageLock});
+// console.log(JSON.stringify(report))
